ba0e40c3cef282c392786737ec8fae76c8a350fe
Kevin Laczkó <kevin.laczko@pomscloud.ie>
2026-06-23 19:26:04 +0200

    Refactor main function to parse and display news article titles and descriptions

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ def main():
     api_key = "1662958beb314cf4b178ad67ccb4256a"
     url = f"https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey={api_key}"
 
-    req = requests.get(url)
-    print(req.text)
+    req = requests.get(url).json()
+    for article in req["articles"]:
+        print(article["title"])
+        print(article["description"])
 
 if __name__ == "__main__":
     main()
