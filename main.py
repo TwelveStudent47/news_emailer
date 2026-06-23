472197f540e18dc8266039f4b172665f191678e4
Kevin Laczkó <kevin.laczko@pomscloud.ie>
2026-06-23 17:18:55 +0200

    Add main.py to fetch news articles from NewsAPI

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+
+def main():
+    api_key = "1662958beb314cf4b178ad67ccb4256a"
+    url = f"https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey={api_key}"
+
+    req = requests.get(url)
+    print(req.text)
+
+if __name__ == "__main__":
+    main()
