48183d4b4a17f205a29e5a15df2ec754b4e103df
Kevin Laczkó <kevin.laczko@pomscloud.ie>
2026-06-23 19:41:41 +0200

    feat: Implement news article fetching and email sending functionality

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import requests
+from send_email import send_email
 
 def main():
     api_key = "1662958beb314cf4b178ad67ccb4256a"
     url = f"https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey={api_key}"
+    topic = []
 
     req = requests.get(url).json()
     for article in req["articles"]:
-        print(article["title"])
-        print(article["description"])
+        topic.append({"title" : article["title"], "description" : article["description"]})
+    
+    send_email(topic)
 
 if __name__ == "__main__":
     main()
